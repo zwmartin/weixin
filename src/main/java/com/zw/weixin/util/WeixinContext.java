766a822cb4d87998af39cfe0ffbe7155a3603b58
martin_zou <360130892@qq.com>
2016-08-23 18:06:17 +0800

    修改系统启动 weixinContext初始化

--- src/main/java/com/zw/weixin/util/WeixinContext.java
+++ src/main/java/com/zw/weixin/util/WeixinContext.java
@@ -6,11 +6,18 @@ import org.springframework.stereotype.Component;
  * 存放微信ACCESS_TOKEN
  * 
  * @author Administrator
- *
+ * 
  */
 @Component
 public class WeixinContext {
 	private String access_Token;
+
+	/** 微信接入TOKEN */
+	private String TOKEN;
+	/** 微信测试公众号APPID */
+	private String APPID;
+	/** 微信测试公众号AppSecret */
+	private String APPSECRET;
 
 	public void setAccessToken(String accessToken) {
 		this.access_Token = accessToken;
@@ -18,5 +25,37 @@ public class WeixinContext {
 
 	public String getAccessToken() {
 		return access_Token;
+	}
+
+	public String getAccess_Token() {
+		return access_Token;
+	}
+
+	public void setAccess_Token(String access_Token) {
+		this.access_Token = access_Token;
+	}
+
+	public String getTOKEN() {
+		return TOKEN;
+	}
+
+	public void setTOKEN(String tOKEN) {
+		TOKEN = tOKEN;
+	}
+
+	public String getAPPID() {
+		return APPID;
+	}
+
+	public void setAPPID(String aPPID) {
+		APPID = aPPID;
+	}
+
+	public String getAPPSECRET() {
+		return APPSECRET;
+	}
+
+	public void setAPPSECRET(String aPPSECRET) {
+		APPSECRET = aPPSECRET;
 	}
 }
